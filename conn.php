515dba3ba8fbb8bfcc8e86f632e208435cbc2ac9
Gabriel Calderón <gabrielvilleda8@gmail.com>
2024-09-17 19:39:56 -0600

    feat: :sparkles: add conection with database

---
+++ conn.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class conexion
+{
+
+    public $host = '127.0.0.1';
+    public $db = 'rrhh';
+    public $user = 'root';
+    public $pass = '';
+    public $port = '3306';
+    public $charset = 'utf8mb4';
+    public $options = [
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+        \PDO::ATTR_EMULATE_PREPARES => false
+    ];
+
+
+    public function conectar()
+    {
+        try {
+            $pdo = new PDO('mysql');
+            return $pdo;
+        } catch (PDOEXCEPTION $exp) {
+            echo ("HUBO UN ERROR EN LA CONEXION");
+        }
+    }
+}
